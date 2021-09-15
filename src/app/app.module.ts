0a4af5f01dede42937f1297daa7510b6c8a81709
faxila <faxila3@gmail.com>
2021-09-15 17:31:48 +0300

    Added component contact header. Added reset, variables scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactHeaderComponent } from './contacts/components/contact-header/contact-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactHeaderComponent
   ],
   imports: [
     BrowserModule
