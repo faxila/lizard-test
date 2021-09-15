0ac6e254ffd285146242f52f21c44c4cceca477f
faxila <faxila3@gmail.com>
2021-09-15 19:57:49 +0300

    added contact-filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactHeaderComponent } from './contacts/components/contact-header/contact-header.component';
+import { ContactFilterComponent } from './contacts/components/contact-filter/contact-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactHeaderComponent
+    ContactHeaderComponent,
+    ContactFilterComponent
   ],
   imports: [
     BrowserModule
